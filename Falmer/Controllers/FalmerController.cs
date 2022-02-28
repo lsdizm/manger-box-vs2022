6094c88582bda4f278045d4020679ee8e4264565
wtg-home <lsdizm@naver.com>
2022-02-28 15:13:48 +0900

    신규 세팅

---
+++ Falmer/Controllers/FalmerController.cs
@@ -0,0 +1,19 @@
+﻿using Falmer.Model.KRA;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Falmer.Controllers
+{
+    [ApiController]
+    [Route("falmer")]
+    public class FalmerController : ControllerBase
+    {
+        [HttpPost("kra", Name = "UpdateKRAData")]
+        public IEnumerable<GameResult> UpdateKRAData()
+        {
+            var result = new List<GameResult>();
+            result.Add(new GameResult() { });
+
+            return result;
+        }
+    }
+}
---
+++ Falmer/Model/KRA/GameResult.cs
@@ -0,0 +1,9 @@
+﻿namespace Falmer.Model.KRA
+{
+    public class GameResult
+    {
+        public string Date { get; set; }
+        public int Sequence { get; set; }
+        public string Remark { get; set; }
+    }
+}
